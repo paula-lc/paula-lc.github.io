b4849d64671c98aec5d82a13a164e874deb036f8
paula-lc <51693088+paula-lc@users.noreply.github.com>
2024-04-27 04:31:00 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,14 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+// email validation
+function validateEmail(emailField){
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  if (reg.test(emailField.value) == false) 
+  {
+      alert('❌ Invalid Email Address ❌');
+      return false;
+  }
+  return true;
+}
